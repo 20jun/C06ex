8409631c88dc0b98a2cefbc5be73a50c4791cdf9
Youngjun Lee <youngjle@c8r6s10.42seoul.kr>
2020-11-03 23:19:40 +0900

    add ex00 ex01 ex02 ing ex03

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngjle <youngjle@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/03 01:25:47 by youngjle          #+#    #+#             */
+/*   Updated: 2020/11/03 01:41:51 by youngjle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc > 0)
+		ft_putchar(argv[0]);
+}
---
+++ ex01/ft_print_params.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngjle <youngjle@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/03 01:45:01 by youngjle          #+#    #+#             */
+/*   Updated: 2020/11/03 01:51:53 by youngjle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int		main(int argc, char *argv[])
+{
+	int i;
+
+	i = 1;
+	if (argc > 0)
+	{
+		while (argv[i] != '\0')
+		{
+			ft_putchar(argv[i]);
+			i++;
+		}
+	}
+}
